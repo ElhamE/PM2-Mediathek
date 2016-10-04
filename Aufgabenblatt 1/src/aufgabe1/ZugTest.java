23c52f9abe0b5f4d4e897fb335a820f407cda6c9
aca483 <aca483@LONDON.INFORMATIK>
2016-10-04 17:03:41 +0200

    Abnahme

--- Aufgabenblatt 1/src/aufgabe1/ZugTest.java
+++ Aufgabenblatt 1/src/aufgabe1/ZugTest.java
@@ -7,48 +7,47 @@ import org.junit.Test;
 
 /**
  * @author Imon Bashir, Elham Esmat
- *
  */
 public class ZugTest
 {
-	private Zug _zug;
-	
-	public ZugTest()
-	{
-		_zug = new Zug(new Lokomotive(10));
-	}
-	
-	@Test
-	/**
-	 * Testet den Konstruktor dieser Klasse.
-	 */
-	public void testKonstruktor()
-	{
-		assertNotNull(_zug);
-	}
-	
-	private Wagon getWagon()
-	{
-		return new Personenwagon(15);
-	}
-	
-	@Test
-	public void TestGibZugbeschreibung1()
-	{
-		Wagon wagon = getWagon();
-		_zug.haengeAn(wagon);
-		assertEquals(
-				" | Lokomotive mit Zugkraft: 10 | Personenwagon mit 15 Sitzplätzen",
-				_zug.gibZugbeschreibung1());
-	}
-	
-	@Test
-	public void TestGibZugbeschreibung2()
-	{
-		Wagon wagon = getWagon();
-		_zug.haengeAn(wagon);
-		assertEquals(
-				" | Lokomotive mit Zugkraft: 10 | Personenwagon mit 15 Sitzplätzen",
-				_zug.gibZugbeschreibung1());
-	}
+    private Zug _zug;
+
+    public ZugTest()
+    {
+        _zug = new Zug(new Lokomotive(10));
+    }
+
+    @Test
+    /**
+     * Testet den Konstruktor dieser Klasse.
+     */
+    public void testKonstruktor()
+    {
+        assertNotNull(_zug);
+    }
+
+    private Wagon getWagon()
+    {
+        return new Personenwagon(15);
+    }
+
+    @Test
+    public void TestGibZugbeschreibung1()
+    {
+        Wagon wagon = getWagon();
+        _zug.haengeAn(wagon);
+        assertEquals(
+                " | Lokomotive mit Zugkraft: 10 | Personenwagon mit 15 Sitzplätzen",
+                _zug.gibZugbeschreibung1());
+    }
+
+    @Test
+    public void TestGibZugbeschreibung2()
+    {
+        Wagon wagon = getWagon();
+        _zug.haengeAn(wagon);
+        assertEquals(
+                " | Lokomotive mit Zugkraft: 10 | Personenwagon mit 15 Sitzplätzen",
+                _zug.gibZugbeschreibung1());
+    }
 }
